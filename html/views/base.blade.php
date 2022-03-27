798a2290f4c7efb2dbaa8b9d6ddc1b25bdc5711d
ukwhatn <ukwhatn@gmail.com>
2022-03-27 12:10:02 +0900

    JQuery,Bootstrapのimportをheadに移動

--- html/views/base.blade.php
+++ html/views/base.blade.php
@@ -9,11 +9,26 @@
     <title>@yield("pageTitle") @yield("siteName")</title>
 
 
-    @yield("siteInformation")
+@yield("siteInformation")
 
-    @yield("pageInformation")
+@yield("pageInformation")
 
-    @yield("headScripts")
+
+<!-----------
+  Scripts Import
+  ----------->
+    <!--JQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"
+    ></script>
+    <!--BootStrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+    ></script>
+
+@yield("headScripts")
 
 <!-----------
       CSS Import
@@ -54,16 +69,6 @@
     <!-----------
       Scripts Import
       ----------->
-    <!--JQuery-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-            crossorigin="anonymous"
-    ></script>
-    <!--BootStrap-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"
-    ></script>
     <!--SWUP Scripts-->
     <script src="https://unpkg.com/swup@latest/dist/swup.min.js"></script>
     <script src="/assets/components/swup/scripts.js"></script>
